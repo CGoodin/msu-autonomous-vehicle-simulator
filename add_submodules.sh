73f11eb2efb068606cf6d17de4b5591d9be07188
Chris Goodin <cgoodin@cavs.msstate.edu>
2024-12-09 13:11:36 -0600

    Updated submodule definitions

---
+++ add_submodules.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+git submodule add https://github.com/dtschump/CImg.git extern/CImg
+git submodule add git://git.code.sf.net/p/tinyfiledialogs/code extern/TinyFileDialogs
+git submodule add -b v3.6.1 https://github.com/embree/embree.git extern/embree
+git submodule add https://github.com/Tencent/rapidjson.git extern/rapidjson
+git submodule add https://github.com/DanielChappuis/reactphysics3d.git extern/reactphysics3d
